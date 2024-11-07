0c63e778c660583b5f4a59818aee7f01a5c1eba1
Jesto9 <82868389+Jesto9@users.noreply.github.com>
2024-11-07 16:49:13 +0300

    Added pictures for new maps

--- Constants/Gallery.js
+++ Constants/Gallery.js
@@ -11,6 +11,8 @@ module.exports = Object.freeze({
     "#mp_rr_olympus_mu1_night": "olympus-after-dark",
 
     // Normal Map
+    "#mp_rr_canyonlands_dayzero": "kings-canyon",
+    "#mp_rr_district": "https://imgpile.com/p/M54aQ8O#A1zn8JD",
     "#mp_rr_canyonlands": "kings-canyon",
     "#mp_rr_desertlands": "world-edge",
     "#mp_rr_olympus": "olympus",
@@ -18,13 +20,14 @@ module.exports = Object.freeze({
     "#mp_rr_divided_moon": "broken-moon",
 
     // Arena Map
+    "#mp_rr_thunderdome": "https://imgpile.com/p/M54aQ8O#A1zn8JD",
     "#mp_rr_arena_phase_runner": "phase-runner",
     "#mp_rr_party_crasher": "party-crasher",
     "#mp_rr_aqueduct": "overflow",
     "#mp_rr_arena_skygarden": "encore",
     "#mp_rr_arena_composite": "drop-off",
     "#mp_rr_arena_habitat": "habitat",
-    "#mp_rr_freedm_map_name_the_core": "the-core",
+    "#mp_rr_freedm_map_name_the_core": "https://imgpile.com/p/Ci3wfPb#IaiCS9X",
 
     // Control Map
     "#control_map_name_lab": "hammond-labs",
@@ -33,9 +36,10 @@ module.exports = Object.freeze({
     "#control_map_name_production": "production",
 
     // Free DM
+    "#freedm_map_name_fragment": "https://imgpile.com/p/wjW7Ttw#slCKYfz",
     "#freedm_map_name_skulltown": "skull-town",
     "#freedm_map_name_estates": "estates",
     "#freedm_map_name_dome": "dome",
     "#freedm_map_name_wattson": "wattsons-pylon",
-    "#freedm_map_name_amps": "amps",
+    "#freedm_map_name_amps": "https://imgpile.com/p/C7E45ip#XwVll5b",
 });
